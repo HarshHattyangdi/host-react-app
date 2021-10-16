3a7de36f9ed7fcf9dd1638e172ae782598a222b5
Harshal <harshal.2797@gmail.com>
2021-10-16 09:29:58 +0530

    Added infinite scroll to the content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,25 +1,38 @@
 import React, { useEffect, useState } from "react";
 import User from "./User";
 import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function Content() {
   const [users, setUsers] = useState([]);
+  const [count] = useState(10);
   useEffect(() => {
     axios
-      .get(`https://randomuser.me/api/?results=10&nat=us,dk,fr,gb`)
+      .get(`https://randomuser.me/api/?results=${count}&nat=us,dk,fr,gb`)
       .then((res: any) => setUsers(res.data.results));
   }, []);
 
+  const fetchUsers = () => {
+    axios
+      .get(`https://randomuser.me/api/?results=${count}&nat=us,dk,fr,gb`)
+      .then((res: any) => setUsers(users.concat(res.data.results)));
+  };
+
   return (
-    console.log(users),
-    (
-      <div>
-        <h2>Inside Contents Page</h2>
+    // console.log(users),
+    <div>
+      <h2>Inside Contents Page</h2>
+      <InfiniteScroll
+        dataLength={users.length}
+        next={fetchUsers}
+        hasMore={true}
+        loader={<h4>Loading.....</h4>}
+      >
         {users.map((user: any) => (
           <User key={user.login.uuid} user={user} />
         ))}
-      </div>
-    )
+      </InfiniteScroll>
+    </div>
   );
 }
 
