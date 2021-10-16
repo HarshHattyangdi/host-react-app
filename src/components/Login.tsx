4e80faff15ce44093b904b39d12ce6aa78129f61
Harshal <harshal.2797@gmail.com>
2021-10-16 13:37:00 +0530

    Login works on form submit

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,21 +1,100 @@
 import React from "react";
 import auth from "./Auth";
+// import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { withRouter, useHistory } from "react-router";
+
+const theme = createTheme();
+
 const Login = (props: any) => {
+  let history = useHistory();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    auth.login(() => {
+      history.push("/content");
+    });
+  };
   return (
-    <div>
-      <input type="text" placeholder="Enter Username" />
-      <input type="password" placeholder="Enter Password" />
-      <input
-        type="Submit"
-        value="Login"
-        onClick={() => {
-          auth.login(() => {
-            props.history.push("/content");
-          });
-        }}
-      />
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="username"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              // onClick={() => {
+              //   auth.login(() => {
+              //     props.history.push("/content");
+              //   });
+              // }}
+            >
+              Login
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+    // <div>
+    //   <form
+    //     onSubmit={() => {
+    //       console.log("calling auth");
+
+    //       // auth.login(() => {
+    //       //   props.history.push("/content");
+    //       // });
+    //     }}
+    //   >
+    //     <input type="text" placeholder="Enter Username" required />
+    //     <input type="password" placeholder="Enter Password" required />
+    //     <input type="Submit" value="Login" />
+    //   </form>
+    // </div>
   );
 };
 
-export default Login;
+export default withRouter(Login);
